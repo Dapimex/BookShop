c34ce66ea952853cc921d03b69ae4214150ef200
Dapimex <denis-pimenov2011@yandex.ru>
2020-02-25 13:14:51 +0300

    Form and Book constructors

--- src/Book.java
+++ src/Book.java
@@ -1,9 +1,9 @@
 public abstract class Book {
 
-    String title;
-    String author;
-    double cost;
-    BookType type;
+    private String title;
+    private String author;
+    private double cost;
+    private BookType type;
 
     public Book(String title, String author, double cost, BookType type) {
         this.title = title;
@@ -12,4 +12,19 @@ public abstract class Book {
         this.type = type;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public BookType getType() {
+        return type;
+    }
 }
--- src/Form.java
+++ src/Form.java
@@ -1,2 +1,24 @@
 public class Form {
+
+    private Book book;
+    private Customer customer;
+    private int days;
+
+    public Form(Book book, Customer customer, int days) {
+        this.book = book;
+        this.customer = customer;
+        this.days = days;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public int getDays() {
+        return days;
+    }
 }

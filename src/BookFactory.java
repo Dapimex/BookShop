83bd5d35271329c0c3f32ff89e19838491446e1e
Dapimex <denis-pimenov2011@yandex.ru>
2020-02-25 12:49:13 +0300

    Book Factory

--- src/BookFactory.java
+++ src/BookFactory.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookFactory {
+
+    List<Book> books = new ArrayList<>();
+    protected int ranobeBookCounter;
+    protected int fantasyBookCounter;
+    protected int detectiveBookCounter;
+    protected int thrillerBookCounter;
+
+    public Book getBook(String title, String author, double cost, BookType type) {
+        Book newBook = null;
+        switch (type) {
+            case RANOBE:
+                ranobeBookCounter++;
+                newBook = new Ranobe(title, author, cost, type);
+                break;
+            case FANTASY:
+                fantasyBookCounter++;
+                newBook = new Fantasy(title, author, cost, type);
+                break;
+            case THRILLER:
+                thrillerBookCounter++;
+                newBook = new Thriller(title, author, cost, type);
+                break;
+            case DETECTIVE:
+                detectiveBookCounter++;
+                newBook = new Detective(title, author, cost, type);
+                break;
+            default:
+                throw new IllegalArgumentException("Wrong book type " + type);
+        }
+        books.add(newBook);
+        return newBook;
+    }
+
 }
